86b255939ede08bf83dfc5bddccbb6c6f251df21
Edwin-SB <57232647+Edwin-SB@users.noreply.github.com>
2020-05-21 10:22:09 -0600

    V3
    se le agrega la escritura en la microSD

--- TCU_Secador_Cacao/TCU_Secador_Cacao.ino
+++ TCU_Secador_Cacao/TCU_Secador_Cacao.ino
@@ -46,27 +46,66 @@ void setup(){
       dataLog.close();                              // Close the file
     }
   }
- 
-  // if the file opened okay, write to it:
-  microSD();
+  
 }//setup
  
-void loop()
-{
-  // nothing happens after setup
+void loop(){ 
+  
+  setTimer();
+  DS3231_display();
+  microSD(); 
 }//loop
 
 void microSD(){
-  if (dataLog) {
-    Serial.print("Writing to test.txt...");
-    dataLog.println("hola");
-  // close the file:
-    dataLog.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+  if(previous_second != second){
+    previous_second = second;
+    // Read humidity
+    RH = dht.readHumidity() * 10;
+    //Read temperature in degree Celsius
+    Temp = dht.readTemperature() * 10;
+    if(Temp < 0){
+      temperature[0] = '-';                     // If temperature < 0 put minus sign
+      Temp = abs(Temp);                         // Absolute value of 'Temp'
+    }
+    else
+      temperature[0] = ' ';                     // otherwise (temperature > 0) put space
+    temperature[1]   = (Temp / 100) % 10  + 48;
+    temperature[2]   = (Temp / 10)  % 10  + 48;
+    temperature[4]  =  Temp % 10 + 48;
+    if(RH >= 1000)
+      humidity[0]    = '1';                     // If humidity >= 100.0% put '1' of hundreds
+    else
+      humidity[0]    = ' ';                     // otherwise (humidity < 100) put space
+    humidity[1]      = (RH / 100) % 10 + 48;    
+    humidity[2]      = (RH / 10) % 10 + 48;
+    humidity[4]     =  RH % 10 + 48;
+    lcd.setCursor(6, 2);
+    lcd.print(temperature);
+    lcd.setCursor(6, 3);
+    lcd.print(humidity);
+    // Send data to Arduino IDE serial monitor
+    Serial.print(Calendar);
+    Serial.print(" | ");
+    Serial.print(Time);
+    Serial.print(" |   ");
+    Serial.print(temperature);
+    Serial.print("°C   |  ");
+    Serial.println(humidity);
+    if(sd_ok) {                                       // If SD card initialization was OK
+      dataLog = SD.open("Logger.txt", FILE_WRITE);    // Open file Logger.txt
+      if(dataLog) {                                   // if the file opened okay, write to it:
+        dataLog.print(Calendar);
+        dataLog.print(" | ");
+        dataLog.print(Time);
+        dataLog.print(" |   ");
+        dataLog.print(temperature);
+        dataLog.print("°C   | ");
+        dataLog.println(humidity);
+        dataLog.close();                              // Close the file
+      }
+    }
   }
+  delay(50); 
   
   }//microSD
 
@@ -138,4 +177,46 @@ void DS3231_display(){
   lcd.print(Time);                               // Display time
   lcd.setCursor(5, 1);
   lcd.print(Calendar);                           // Display calendar
-}
+}// ds3231 display
+
+void setTimer(){
+if(!digitalRead(B1)){                           // If button (pin #8) is pressed
+    i = 0;
+    hour   = edit(5, 0, hour);
+    minute = edit(8, 0, minute);
+    date   = edit(5, 1, date);
+    month  = edit(8, 1, month);
+    year   = edit(13, 1, year);
+    // Convert decimal to BCD
+    minute = ((minute / 10) << 4) + (minute % 10);
+    hour = ((hour / 10) << 4) + (hour % 10);
+    date = ((date / 10) << 4) + (date % 10);
+    month = ((month / 10) << 4) + (month % 10);
+    year = ((year / 10) << 4) + (year % 10);
+    // End conversion
+    // Write data to DS3231 RTC
+    Wire.beginTransmission(0x68);               // Start I2C protocol with DS3231 address
+    Wire.write(0);                              // Send register address
+    Wire.write(0);                              // Reset sesonds and start oscillator
+    Wire.write(minute);                         // Write minute
+    Wire.write(hour);                           // Write hour
+    Wire.write(1);                              // Write day (not used)
+    Wire.write(date);                           // Write date
+    Wire.write(month);                          // Write month
+    Wire.write(year);                           // Write year
+    Wire.endTransmission();                     // Stop transmission and release the I2C bus
+    delay(200);                                 // Wait 200ms
+  }
+  
+  Wire.beginTransmission(0x68);                 // Start I2C protocol with DS3231 address
+  Wire.write(0);                                // Send register address
+  Wire.endTransmission(false);                  // I2C restart
+  Wire.requestFrom(0x68, 7);                    // Request 7 bytes from DS3231 and release I2C bus at end of reading
+  second = Wire.read();                         // Read seconds from register 0
+  minute = Wire.read();                         // Read minuts from register 1
+  hour   = Wire.read();                         // Read hour from register 2
+  Wire.read();                                  // Read day from register 3 (not used)
+  date   = Wire.read();                         // Read date from register 4
+  month  = Wire.read();                         // Read month from register 5
+  year   = Wire.read();                         // Read year from register 6
+}// setTimer
